5bc884698af45cf6a04942edd3ae0ee518247d54
SAX_94 <matteosacchi94@gmail.com>
2022-05-24 20:06:45 +0200

    created part of script for random surnames. it works

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -2,13 +2,26 @@
 const nomi = ["Aldo", "Marco", "Mario", "Luca", "Fabio"];
 const cognomi = ["Verdi", "Rossi", "Bianchi", "Brambilla", "Greco"];
 
+/* Assegno un indice uguale a 0 a nomi e cognomi prima 
+di far partire il ciclo. Prima della partenza del ciclo, 
+nomeRandom e cognomeRandom sono stringhe vuote. */
+
 let nomeIndex = 0;
 let cognomeIndex = 0;
 let nomeRandom = "";
 let cognomeRandom = "";
 
-for (let i = 0; i < nomi.length; i++) {
-    nomeIndex = parseInt(Math.floor(Math.random() *  nomi.length));
+for (let i = 0; i <= nomi.length; i++) {
+    nomeIndex = parseInt(Math.floor(Math.random() * nomi.length));
     nomeRandom = nomi[nomeIndex];
     console.log(nomeRandom);
 }
+
+for (let i = 0; i <= cognomi.length; i++) {
+    cognomeIndex = parseInt(Math.floor(Math.random() * cognomi.length));
+    cognomeRandom = cognomi[cognomeIndex];
+    console.log(cognomeRandom);
+}
+
+// STAMPO LA SOMMA DI NOMI E COGNOMI RANDOM
+
